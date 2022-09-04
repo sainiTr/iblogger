3b1ed1f67a00d5c4e7e5c1f31ee84c2c5cf1f339
Tenali Ramkrishna <112521200+sainiTr@users.noreply.github.com>
2022-09-04 17:06:20 +0530

    re

--- mydailycode/settings.py
+++ mydailycode/settings.py
@@ -124,11 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join( BASE_DIR / "mydailycode/static"),
-    os.path.join( BASE_DIR / "Html/static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join( BASE_DIR / "mydailycode/static"),
+#     os.path.join( BASE_DIR / "Html/static")
+# ]
 
+STATIC_ROOT = os.path.join( BASE_DIR / "static/")
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
