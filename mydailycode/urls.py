198907fb791b2110dba44df96050af41062e95a8
Tenali Ramkrishna <112521200+sainiTr@users.noreply.github.com>
2022-09-04 22:08:05 +0530

    c

--- mydailycode/urls.py
+++ mydailycode/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='Home'),
@@ -28,6 +28,8 @@ urlpatterns = [
     # path('html-course/<str:page>/',include('Html.urls')),
     path('python-course/',include('python.urls')),
     path('html-course/<str:page>/',include('Html.urls')),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
