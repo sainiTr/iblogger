ba93d399f1c803c005eb5055695f7fc738e7dafe
Tenali Ramkrishna <112521200+sainiTr@users.noreply.github.com>
2022-09-04 16:55:20 +0530

    Update settings.py

--- mydailycode/settings.py
+++ mydailycode/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join( BASE_DIR / "static")
+    os.path.join( BASE_DIR / "mydailycode/static"),
+    os.path.join( BASE_DIR / "Html/static")
 ]
 
 
